60b2e79e1e7ae40348c28d0e0ae7f4280782d03d
Teri Eyenike <teyenike1@gmail.com>
2021-09-11 18:19:51 +0100

    header footer section added

---
+++ sections/Footer.js
@@ -0,0 +1,13 @@
+const Footer = () => {
+  return (
+    <>
+      <footer className="px-4 py-6 mt-24 sm:px-6">
+        <p className="text-sm text-center text-gray-500">
+          &copy; {new Date().getFullYear()} Teri. All rights reserved
+        </p>
+      </footer>
+    </>
+  );
+};
+
+export default Footer;
