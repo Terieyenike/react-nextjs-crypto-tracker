f0a96797d29e8ca343ec061486586df950e371b9
Teri Eyenike <teyenike1@gmail.com>
2021-09-11 18:20:19 +0100

    components for cryption exchange page

---
+++ components/SearchBar/index.js
@@ -0,0 +1,14 @@
+const SearchBar = ({ ...rest }) => {
+  return (
+    <>
+      <div className="flex items-center m-16 w-80">
+        <input
+          className="w-full h-full p-4 bg-gray-200 border-transparent rounded"
+          {...rest}
+        />
+      </div>
+    </>
+  );
+};
+
+export default SearchBar;
