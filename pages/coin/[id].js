2c22892a36c38ae5c36caa6ce6834d8577703f34
Teri Eyenike <teyenike1@gmail.com>
2021-09-11 23:35:42 +0100

    coin individual dynamic page

--- pages/coin/[id].js
+++ pages/coin/[id].js
@@ -9,18 +9,12 @@ const Coin = ({ coin }) => {
       pageMeta={{
         title: `Crypto Coin ${coin.id}`,
       }}>
-      <div>
-        <div>
-          <Image
-            src={fallback}
-            alt={coin.name}
-            width={30}
-            height={30}
-            className="mr-4"
-          />
-          <h1>{coin.name}</h1>
-          <p>{coin.symbol}</p>
-          <p>{coin.market_data.current_price.usd}</p>
+      <div className="flex justify-center h-1/6">
+        <div className="flex flex-col items-center justify-center w-1/4 p-16 border border-purple-500 border-solid rounded-lg">
+          <Image src={fallback} alt={coin.name} width={200} height={200} />
+          <h1 className="mt-8 mb-4">{coin.name}</h1>
+          <p className="mb-4 uppercase">{coin.symbol}</p>
+          <p className="mb-4 text-3xl">${coin.market_data.current_price.usd}</p>
         </div>
       </div>
     </Layout>
