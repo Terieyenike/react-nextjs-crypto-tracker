9e7821007abe7186ed7f776fa75e14e53bff3be9
Teri Eyenike <teyenike1@gmail.com>
2021-09-11 23:00:03 +0100

    integrated the pages and components to the crypto home page

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,42 @@
-import Head from 'next/head';
-import Image from 'next/image';
+import { useState } from 'react';
+import CoinList from '../components/CoinList';
 import SearchBar from '../components/SearchBar';
 import Layout from '../sections/Layout';
 
-export default function Home() {
+export default function Home({ filteredCoins }) {
+  const [search, setSearch] = useState('');
+
+  const allCoins = filteredCoins.filter((coin) =>
+    coin.name.toLowerCase().includes(search.toLowerCase())
+  );
+
+  const handleChange = (e) => {
+    e.preventDefault();
+
+    setSearch(e.target.value.toLowerCase());
+  };
+
   return (
     <div>
       <Layout>
         <section className="flex flex-col items-center m-2.5">
-          <SearchBar type="text" placeholder="Search" />
+          <SearchBar type="text" placeholder="Search" onChange={handleChange} />
+          <CoinList filteredCoins={allCoins} />
         </section>
       </Layout>
     </div>
   );
 }
+
+export const getServerSideProps = async () => {
+  const url =
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false';
+  const res = await fetch(url);
+  const filteredCoins = await res.json();
+
+  return {
+    props: {
+      filteredCoins,
+    },
+  };
+};
--- sections/Footer.js
+++ sections/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <>
-      <footer className="px-4 py-6 mt-24 sm:px-6">
+      <footer className="py-6 mt-12 sm:px-6">
         <p className="text-sm text-center text-gray-500">
           &copy; {new Date().getFullYear()} Teri. All rights reserved
         </p>
