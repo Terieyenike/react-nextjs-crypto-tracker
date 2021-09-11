7c28de7bf67218d55897fe9af79251b5edf95ffa
Teri Eyenike <teyenike1@gmail.com>
2021-09-11 18:20:47 +0100

    crypto exchange page

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
 module.exports = {
-  purge: [],
+  mode: 'jit',
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './sections/**/*.{js,ts,jsx,tsx}',
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
@@ -8,4 +13,4 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-}
+};
